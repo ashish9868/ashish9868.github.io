81a60f82d47c5bf0b8ead90738a199fa5fcd2762
Ashish <akhanna@vidatec.com>
2022-10-18 16:20:41 +0530

    more samples

--- main.js
+++ main.js
@@ -1,8 +1,53 @@
 (function () {
     var BrowserFeatureUtility = function (config = {}) {
+        var dummyUrl = "https://example.com"
+        var soundUrl = "/sample.mp3"
+        var modalInformation = $('#modalInformation')
+        var modelConfirmation = $('#modalConfirmation')
         this.config = Object.assign({}, config)
+        this.testResults = [];
         this.errors = []
-        this.isTestValid = function (test) {
+        this.toggleInfoModal = function (show, title, message, callbackOk) {
+            var modelTitle = modalInformation.find('.modal-title')
+            var modelDescription = modalInformation.find('.modal-body p')
+            var okButton = modalInformation.find('button.okButton')
+            if (show) {
+                modelTitle.html(title)
+                modelDescription.html(message)
+                okButton.on('click', function () {
+                    callbackOk()
+                })
+                modalInformation.modal('show')
+            } else {
+                modelTitle.html('')
+                modelDescription.html('')
+                modalInformation.modal('hide')
+            }
+        };
+        this.toggleConfirmationModal = function (show, title, message, callback) {
+            var modelTitle = modelConfirmation.find('.modal-title')
+            var modelDescription = modelConfirmation.find('.modal-body p')
+            var okButton = modelConfirmation.find('button.okButton')
+            var cancelButton = modelConfirmation.find('button.cancelButton')
+            if (show) {
+                modelTitle.html(title)
+                modelDescription.html(message)
+                okButton.on('click', function () {
+                    callback(true)
+                })
+                cancelButton.on('click', function () {
+                    callback(false)
+                })
+                modelConfirmation.modal('show')
+            } else {
+                modelTitle.html('')
+                modelDescription.html('')
+                modelConfirmation.modal('hide')
+            }
+        }
+        this.peformTest = function (data) {
+            var index = data.index
+            var test = data.test
             var self = this;
             if (!test || typeof test !== "object") {
                 return false;
@@ -10,11 +55,68 @@
             switch (test.type) {
                 case 'VALIDATE_URL':
                     if (test.url && test.name) {
-                        return true;
+                        self.validateUrl(test.url, function (xhr) {
+                            self.testResults[index] = Object.assign(test, {
+                                result: xhr.status + "".indexOf("20") != -1,
+                                message: "URL StatusCode: " + xhr.status
+                            })
+                        })
                     } else {
                         self.errors.push("Test: " + test.name + "do not have url or name key.");
-                        return false;
                     }
+                    break;
+                case 'SYSTEMINFO':
+                    var info = self.getSystemInfo()
+                    self.testResults[index] = Object.assign(test, {
+                        result: true,
+                        message: info.systemInfoString,
+                        extra: info,
+                    })
+                    break;
+                case 'SPEAKER':
+                    self.toggleInfoModal(true, "Testing speakers", "You will hear a sound, once contined, please confirm when asked.", function () {
+                        self.toggleInfoModal(false)
+                        self.playSound(2000, function () {
+                            self.toggleConfirmationModal(true, "Heard a sound?", "Please confirm if you've heard a sound?", function (status) {
+                                self.testResults[index] = Object.assign(test, {
+                                    result: status,
+                                    message: "Speakers are " + (status ? "Working" : "Not Working."),
+                                })
+                                self.toggleConfirmationModal(false)
+                            })
+                        })
+                    })
+                    break;
+                case 'MICROPHONE':
+                    self.toggleInfoModal(true, "Testing microphone access", "You will see a permission popup in your browser's left corner, please allow that to continue.", function () {
+                        self.toggleInfoModal(false)
+                        self.checkMicroPhoneAccess(function (status) {
+                            self.testResults[index] = Object.assign(test, {
+                                result: status,
+                                message: "Microphone status: " + (status ? "have access to microphone" : "Either access is denied or system don't have microphone."),
+                            })
+                        })
+                    })
+                    break;
+                case 'CAMERA':
+                    self.toggleInfoModal(true, "Testing camera access", "You will see a permission popup in your browser's left corner, please allow that to continue.", function () {
+                        self.toggleInfoModal(false)
+                        self.checkMicroPhoneAccess(function (status) {
+                            self.testResults[index] = Object.assign(test, {
+                                result: status,
+                                message: "Camera status: " + (status ? "have access to camera" : "Either access is denied or system don't have camera."),
+                            })
+                        })
+                    })
+                    break;
+                case 'POPUP':
+                    var status = self.canOpenPopup()
+                    self.testResults[index] = Object.assign(test, {
+                        result: status,
+                        message: "Popup(s) are " + (status ? "Allowed" : "Not Allowed."),
+                    })
+
+                    break;
                 default:
                     return false;
             }
@@ -34,22 +136,19 @@
                 complete: callback,
             })
         };
-        this.validateCamera = function () {
-
-        };
         this.validateCookies = function () {
             return navigator.cookieEnabled
         };
-        this.browserInfo = function () {
+        this.getSystemInfo = function () {
             return {
                 os: platform.os,
-                osString: [platform.os.family, "(" + platform.os.architecture + " bit)", platform.os.version || ''].join(" ").trim(),
+                systemInfoString: ["Browser: " + platform.name + " | ", platform.os.family, "(" + platform.os.architecture + " bit)", platform.os.version || ''].join(" ").trim(),
                 name: platform.name,
                 version: platform.version
             }
         };
         this.canOpenPopup = function () {
-            const newWin = window.open("https://google.com", "_blank", "toolbar=no,scrollbars=no,resizable=no,top=0,left=0,width=10,height=10");
+            const newWin = window.open(dummyUrl, "_blank", "toolbar=no,scrollbars=no,resizable=no,top=0,left=0,width=10,height=10");
             const opened = !(!newWin || newWin.closed || typeof newWin.closed == "undefined")
             setTimeout(function () {
                 if (opened) {
@@ -58,17 +157,51 @@
             }, 100)
             return opened
         };
-        this.parseTests = function () {
-            var self = this;
-            this.config.tests.forEach(function (test) {
-                self.isTestValid(test)
-            })
+        this.playSound = function (timeMs, callback) {
+            var audio = document.getElementById('audioPlayer') ?? document.createElement('audio')
+            audio.id = "audioPlayer"
+            document.body.appendChild(audio)
+            audio.src = soundUrl
+            audio.autoplay = true;
+            setTimeout(function () {
+                document.body.removeChild(audio)
+                callback()
+            }, timeMs);
         };
-
+        this.checkMicroPhoneAccess = function (callback) {
+            navigator.mediaDevices.getUserMedia({ audio: true })
+                .then(function (stream) {
+                    const tracks = stream.getTracks();
+                    tracks.forEach(function (track) {
+                        track.stop()
+                    })
+                    callback(true)
+                })
+                .catch(function (err) {
+                    callback(false, err)
+                });
+        };
+        this.checkCameraAccess = function (callback) {
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(function (stream) {
+                    const tracks = stream.getTracks();
+                    tracks.forEach(function (track) {
+                        track.stop()
+                    })
+                    callback(true)
+                })
+                .catch(function (err) {
+                    callback(false, err)
+                });
+        };
         this.begin = function () {
-            console.log(this.browserInfo(), this.canOpenPopup())
-            this.parseTests()
-            this.validateCamera()
+            var self = this;
+            this.config.tests.forEach(function (test, index) {
+                self.peformTest({
+                    index: index,
+                    test: test
+                })
+            })
         }
     }
 
